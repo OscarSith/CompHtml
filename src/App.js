8c921fdd9dc161b1a774b8b8695c03aed3cfafec
Oscar Larriega <larriega@gmail.com>
2022-10-07 19:14:04 -0500

    Init project REACT from scratch

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App text-center">
+        <h1>Probando REACT FROM SCRATCH</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
