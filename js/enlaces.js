063f4a7c0305b11fc71577a7fbc6f9325a309f45
Anaclau15 <105999960+Anaclau15@users.noreply.github.com>
2022-10-28 07:15:22 -0500

    Actualización enlaces

--- js/enlaces.js
+++ js/enlaces.js
@@ -1,21 +1,22 @@
 const url_servicio = 'http://localhost/proyecto_prueba/enlaces.php';
+let listaEnlaces = [];
+
 const traerEnlaces = () => {
     fetch(url_servicio).then((response) => {
         return response.json();
     }).then((enlaces) => {
-        
+        listaEnlaces = enlaces;
         const primeros = enlaces.filter((enlace) => {
             if(enlace.parentesco == 0){
                 return enlace;
             }
         })
-        console.log(primeros);
 
         let item_html = "";
 
         for (let i = 0; i < primeros.length; i++) {
             item_html += `<li class="mb-3">
-                            <a href="#" class="text-decoration-none fs-5 text-secondary d-block bg-white p-2">${primeros[i].nombre}</a>
+                            <a href="#${primeros[i].id}" class="text-decoration-none fs-5 text-secondary d-block bg-white p-2">${primeros[i].nombre}</a>
                         </li>`;
             
         }
@@ -27,11 +28,36 @@ const traerEnlaces = () => {
             enlace.addEventListener("click", (event) => {
                 event.preventDefault();
 
+                const enlace = event.target;
                 const seleccionado = event.target.closest("ul").querySelector("a.seleccionado");
                 if (seleccionado != null) {
                     seleccionado.classList.remove("seleccionado");
                 }
-                event.target.classList.add("seleccionado");
+                enlace.classList.add("seleccionado");
+
+                //Obtengo información del atributo href
+                let idLista = enlace.getAttribute("href");
+
+                //Obtener el id
+                idLista = idLista.substr(1);
+
+                //Obtener sub enlaces de los primeros enlaces
+                const subEnlaces = listaEnlaces.filter((enlace) => {
+                    if (enlace.parentesco == idLista) {
+                        return enlace;
+                    }
+                })
+
+                let item_html = "";
+
+                for (let i = 0; i < subEnlaces.length; i++) {
+                    item_html += `<li class="mb-3">
+                                    <a href="#${subEnlaces[i].id}" class="text-decoration-none fs-5 text-secondary d-block bg-white p-2">${subEnlaces[i].nombre}</a>
+                                </li>`;
+                    
+                }
+
+                document.getElementById("sub-lista").innerHTML = item_html;
             })
         })
     });
