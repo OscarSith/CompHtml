ef312c1c6bd0faf8297d5cd00268d58b8cf985ea
Anaclau15 <105999960+Anaclau15@users.noreply.github.com>
2022-11-11 07:49:30 -0500

    Avance administrar mis datos paso 2

--- js/usuario.js
+++ js/usuario.js
@@ -1,8 +1,12 @@
+let data_usuario = null;
+
 const traerUsuarios = () => {
     fetch("http://localhost/proyecto_prueba/usuario.php")
     .then((response) =>{
         return response.json();
     }).then((usuario) => {
+        data_usuario = usuario;
+
         const nombreCompleto = `${usuario.primer_nombre} ${usuario.segundo_nombre} ${usuario.primer_apellido} ${usuario.segundo_apellido}`;
         document.getElementById("nombre-completo").innerText = nombreCompleto;
         document.getElementById("doc-usuario").innerText = usuario.doc_usuario;
@@ -35,7 +39,41 @@ const traerUsuarios = () => {
         document.getElementById("pert-etnica").innerText = usuario.datos_adicionales.pertenencia_etnica;
         document.getElementById("pref-contacto").innerText = usuario.datos_adicionales.pref_contacto;
 
+        document.getElementById("btn-mostrar-form-datos-basicos").addEventListener("click", mostrarFormularioDatosBasicos);
+
+        // TODO: estas lineas iran en la funcion mostrarFormularioDatosBasicos
+        document.getElementById('input-primer-nombre').value = data_usuario.primer_nombre;
+        document.getElementById('input-segundo-nombre').value = data_usuario.segundo_nombre;
+        document.getElementById('input-primer-apellido').value = data_usuario.primer_apellido;
+        document.getElementById('input-segundo-apellido').value = data_usuario.segundo_apellido;
+        document.getElementById('fecha-nacimiento').value = data_usuario.fecha_nac;
+
+        const selectGenero = document.getElementById('select-genero');
+        if (data_usuario.genero == selectGenero.options[0].value) {
+            selectGenero.options[0].selected = true;
+        }else{
+            selectGenero.options[1].selected = true;
+        }
+
+
+        const selectEstadoCivil = document.getElementById('select-estado-civil');
+
+        for (let i = 0; i < selectEstadoCivil.options.length; i++) {
+            const optionValue = selectEstadoCivil.options[i].value;
+
+            if (optionValue == data_usuario.estado_civil) {
+                // selectEstadoCivil.options[i].selected = true;
+                selectEstadoCivil.options.selectedIndex = i;
+            }
+        }
     });
 }
+
+const mostrarFormularioDatosBasicos = (evento) => {
+    document.getElementById("panel-datos-basicos").classList.add("d-none");
+    document.getElementById("form-datos-basicos").classList.remove("d-none");
+
+    document.getElementById('input-primer-nombre').value = data_usuario.primer_nombre;
+};
 
 traerUsuarios();
