1dcbd3d9c25b190dd598610d00581419d490145d
Anaclau15 <105999960+Anaclau15@users.noreply.github.com>
2022-11-28 19:40:01 -0500

    Parte final del stepper

--- js/usuario.js
+++ js/usuario.js
@@ -40,7 +40,14 @@ const traerUsuarios = () => {
         document.getElementById("pref-contacto").innerText = usuario.datos_adicionales.pref_contacto;
 
         document.getElementById("btn-mostrar-form-datos-basicos").addEventListener("click", mostrarFormularioDatosBasicos);
-        document.getElementById("btn-back").addEventListener("click", mostrarContenidoAnterior);
+        document.getElementById("btn-cancelar").addEventListener("click", mostrarContenidoBtnCancelar);
+        document.getElementById("btn-cancelar2").addEventListener("click", mostrarContenidoBtnCancelar);
+        document.getElementById("btn-cancelar3").addEventListener("click", mostrarContenidoBtnCancelar);
+        document.getElementById("btn-anterior").addEventListener("click", mostrarContenidoBtnAnterior);
+        document.getElementById("btn-anterior2").addEventListener("click", mostrarContenidoBtnAnterior2);
+        document.getElementById("btn-paso2").addEventListener("click", mostrarContenidoPaso2);
+        document.getElementById("btn-paso3").addEventListener("click", mostrarContenidoPaso3);
+
         // TODO: estas lineas iran en la funcion mostrarFormularioDatosBasicos
         document.getElementById('input-primer-nombre').value = data_usuario.primer_nombre;
         document.getElementById('input-segundo-nombre').value = data_usuario.segundo_nombre;
@@ -72,12 +79,125 @@ const traerUsuarios = () => {
 const mostrarFormularioDatosBasicos = (evento) => {
     document.getElementById("panel-datos-basicos").classList.add("d-none");
     document.getElementById("form-datos-basicos").classList.remove("d-none");
+    document.querySelector("#wizard-actualizar-datos .col").classList.add("active");
 };
 
-const mostrarContenidoAnterior = (evento) => {
+const mostrarContenidoBtnCancelar = (evento) => {
     //console.log(evento);
     document.getElementById("form-datos-basicos").classList.add("d-none");
     document.getElementById("panel-datos-basicos").classList.remove("d-none");
+}
+
+const mostrarContenidoBtnAnterior = (evento) => {
+    document.getElementById("actualizar-datos-paso1").classList.remove("d-none");
+    document.getElementById("actualizar-datos-paso2").classList.add("d-none");
+    document.querySelector("#wizard-actualizar-datos .col").classList.remove("check-listo");
+    document.querySelector("#wizard-actualizar-datos .col").classList.add("active");
+    document.querySelector("#wizard-actualizar-datos .column2").classList.remove("active");
+}
+
+const mostrarContenidoBtnAnterior2 = (evento) => {
+    document.getElementById("actualizar-datos-paso3").classList.add("d-none");
+    document.getElementById("actualizar-datos-paso2").classList.remove("d-none");
+    document.querySelector("#wizard-actualizar-datos .column2").classList.remove("check-listo");
+    document.querySelector("#wizard-actualizar-datos .column3").classList.remove("active");
+    document.querySelector("#wizard-actualizar-datos .column2").classList.add("active");
+}
+
+const mostrarContenidoPaso2 = (evento) => {
+    document.getElementById("actualizar-datos-paso1").classList.add("d-none");
+    document.getElementById("actualizar-datos-paso2").classList.remove("d-none");
+    document.querySelector("#wizard-actualizar-datos .col").classList.remove("active");
+    document.querySelector("#wizard-actualizar-datos .col").classList.add("check-listo");
+    document.querySelector("#wizard-actualizar-datos .column2").classList.add("active");
+
+    const formElement = document.getElementById("form-actualizar-datos");
+    const formData = new FormData(formElement);
+
+    let objectData = {
+        estado_civil: formData.get("estado_civil"),
+        fecha_nac: formData.get("fecha_nac"),
+        genero: formData.get("genero"),
+        primer_apellido: formData.get("primer_apellido"),
+        primer_nombre: formData.get("primer_nombre"),
+        segundo_apellido: formData.get("segundo_apellido"),
+        segundo_nombre: formData.get("segundo_nombre")
+    };
+
+    let listaDatosModificados = [];
+
+    if (data_usuario.primer_nombre !== objectData.primer_nombre) {
+        listaDatosModificados.push({
+            label: "Primer nombre",
+            value: objectData.primer_nombre
+        });
+    }
+
+    if (data_usuario.segundo_nombre !== objectData.segundo_nombre) {
+        listaDatosModificados.push({
+            label: "Segundo nombre",
+            value: objectData.segundo_nombre
+        });
+    }
+
+    if (data_usuario.primer_apellido !== objectData.primer_apellido) {
+        listaDatosModificados.push({
+            label: "Primer apellido",
+            value: objectData.primer_apellido
+        });
+    }
+
+    if (data_usuario.segundo_apellido !== objectData.segundo_apellido) {
+        listaDatosModificados.push({
+            label: "Segundo apellido",
+            value: objectData.segundo_apellido
+        });
+    }
+
+    if (data_usuario.genero !== objectData.genero) {
+        listaDatosModificados.push({
+            label: "Género",
+            value: objectData.genero
+        });
+    }
+
+    if (data_usuario.estado_civil !== objectData.estado_civil) {
+        listaDatosModificados.push({
+            label: "Estado civil",
+            value: objectData.estado_civil
+        });
+    }
+
+    if (data_usuario.fecha_nac !== objectData.fecha_nac) {
+        listaDatosModificados.push({
+            label: "Fecha nacimiento ",
+            value: objectData.fecha_nac
+        });
+    }
+//console.log(listaDatosModificados);
+
+    let imprimirObjeto = "";
+    for (let i = 0; i < listaDatosModificados.length; i++) {
+        const object = listaDatosModificados[i];   
+        
+        imprimirObjeto +=    `<div class="row">
+                                <div class="col-3 label-dato-actualizado">
+                                    <p>${object.label}</p>
+                                </div>
+                                <div class="col-9">
+                                    <p>${object.value}</p>
+                                </div>
+                            </div>`
+    }
+    document.getElementById("datos-modificados").innerHTML = imprimirObjeto;
+}
+
+const mostrarContenidoPaso3 = (evento) => {
+    document.getElementById("actualizar-datos-paso3").classList.remove("d-none");
+    document.getElementById("actualizar-datos-paso2").classList.add("d-none");
+    document.querySelector("#wizard-actualizar-datos .column2").classList.remove("active");
+    document.querySelector("#wizard-actualizar-datos .column2").classList.add("check-listo");
+    document.querySelector("#wizard-actualizar-datos .column3").classList.add("active");
 }
 
 traerUsuarios();
